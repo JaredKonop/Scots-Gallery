e8b4f880f7a8ca0d19d47a9cd2dc2d13e25a2cc2
Jared <jaredkonop@gmail.com>
2024-07-23 14:50:59 -0400

    Added Gallery

---
+++ scots-gallery/src/components/ImageGallery.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../styles/ImageGallery.css";
+
+const images = [
+  {
+    src: "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?w=500&h=500&fit=crop",
+    alt: "person writing in a notebook beside by an iPad, laptop, printed photos, spectacles, and a cup of coffee on a saucer",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1515260268569-9271009adfdb?w=500&h=500&fit=crop",
+    alt: "sunset behind San Francisco city skyline",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1506045412240-22980140a405?w=500&h=500&fit=crop",
+    alt: "people holding umbrellas on a busy street at night lit by street lights and illuminated signs in Tokyo, Japan",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1514041181368-bca62cceffcd?w=500&h=500&fit=crop",
+    alt: "car interior from central back seat position showing driver and blurred view through windscreen of a busy road at night",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1445810694374-0a94739e4a03?w=500&h=500&fit=crop",
+    alt: "back view of woman wearing a backpack and beanie waiting to cross the road on a busy street at night in New York City, USA",
+  },
+  {
+    src: "https://images.unsplash.com/photo-1486334803289-1623f249dd1e?w=500&h=500&fit=crop",
+    alt: "man wearing a black jacket, white shirt, blue jeans, and brown boots, playing a white electric guitar while sitting on an amp",
+  },
+];
+
+const ImageGallery = () => {
+  return (
+    <div className="container">
+      <h1 className="heading">
+        <span>Galllery </span>
+      </h1>
+      <div className="gallery">
+        {images.map((image, index) => (
+          <div className="gallery-item" key={index}>
+            <img className="gallery-image" src={image.src} alt={image.alt} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageGallery;
--- scots-gallery/src/pages/Work.js
+++ scots-gallery/src/pages/Work.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "../styles/page.css";
+import ImageGallery from "../components/ImageGallery";
+
 // import logo from "../assets/Logo.png";
 
 const Work = () => {
-  return <h1>Work Page</h1>;
+  return (
+    <div className="App">
+      <ImageGallery />
+    </div>
+  );
 };
 
 export default Work;
